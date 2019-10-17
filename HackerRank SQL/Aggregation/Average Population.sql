e2fb4a861e627908c1b43ba5ea6ad88f273314e4
Vaishali Oga <voga@umich.edu>
2019-10-16 22:02:32 -0400

    SQL Hackerrank solutions

---
+++ HackerRank SQL/Aggregation/Average Population.sql
@@ -0,0 +1,9 @@
+
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+
+
+
+
+SELECT ROUND(AVG(POPULATION),2)
+FROM CITY
---
+++ HackerRank SQL/Basic Select/Weather Observation Station 11.sql
@@ -0,0 +1,10 @@
+
+
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U','a','e','i','o','u')
+OR RIGHT(CITY,1) NOT IN ('A','E','I','O','U','a','e','i','o','u')
